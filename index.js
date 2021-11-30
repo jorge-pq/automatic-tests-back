c6568c85d4e11ae451688edbd0587904408fe633
jpozo-1nationgfx <jpozo@1nationgfx.com>
2021-11-30 11:16:04 -0500

    test without bd

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ const {CONFIG} = require("./src/config")
 const routes = require("./src/routes") 
 const cors = require("cors")
 
-const {DB_HOST, DB_PORT, DB_NAME, DB_PASSWORD} = CONFIG;
+// const {DB_HOST, DB_PORT, DB_NAME, DB_PASSWORD} = CONFIG;
 
-mongoose
-	.connect("mongodb+srv://jorge:bfnNlgS5gnhCdZhl@cluster0.cn88n.mongodb.net/booking?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true })
-	//.connect(`mongodb://${DB_HOST}:${DB_PORT}/${DB_NAME}`, { useNewUrlParser: true })
-	.then(() => {
+// mongoose
+// 	.connect("mongodb+srv://jorge:bfnNlgS5gnhCdZhl@cluster0.cn88n.mongodb.net/booking?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true })
+// 	//.connect(`mongodb://${DB_HOST}:${DB_PORT}/${DB_NAME}`, { useNewUrlParser: true })
+// 	.then(() => {
 		const app = express()
 		app.use(cors())
 		app.use(express.json()) 
@@ -18,4 +18,4 @@ mongoose
 		app.listen(5000, () => {
 			console.log("Server has started!")
 		})
-	})
+	// })
