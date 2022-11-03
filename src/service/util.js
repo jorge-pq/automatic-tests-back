a910ec227b8f9ab9ec4151896610419071d72d0d
djorg-solutions <djorg90@gmail.com>
2022-11-03 10:58:28 -0400

    upd: booking code format

--- src/service/util.js
+++ src/service/util.js
@@ -2,7 +2,7 @@ const Booking = require("../models/Booking");
 
 const getCode = async (code) => {
    let count = await Booking.countDocuments({});
-   let result = `${code}-${count+1}`
+   let result = `${code}-${String(count+1).padStart(4, '0')}`
    return result;
 }
 
