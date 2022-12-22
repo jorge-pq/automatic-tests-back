c05b8e2716dc66106ca6d7ee464310b83bd2997b
djorg-solutions <djorg90@gmail.com>
2022-12-22 18:20:58 -0500

    add client edit endpoint

--- src/controllers/clients.js
+++ src/controllers/clients.js
@@ -20,4 +20,33 @@ app.post(path("clients/create"), Auth, async (req, res) => {
 	res.send({message: 'Client added succesfully!'});
 })
 
+app.put(path("clients/:id"), Auth, async (req, res) => {
+	
+	try {
+		const client = await Client.findOne({ _id: req.params.id })
+		
+		client.name = req.body.name;
+		client.secondname = req.body.secondname;
+		client.lastname = req.body.lastname;
+		client.secondlastname = req.body.secondlastname;
+		client.phone = req.body.phone;
+		client.email = req.body.email;
+		client. birthday = req.body.birthday;
+		client.clientID =  req.body.clientID;
+		client.state = req.body.state;
+		client.city = req.body.city;
+		client.address = req.body.address;
+		client.zipcode = req.body.zipcode;
+
+
+		await client.save()
+		res.send(client)
+	} catch(err) {
+		console.log(err)
+		res.status(404)
+		res.send({ error: "Client doesn't exist!" })
+	}
+})
+
+
 }
