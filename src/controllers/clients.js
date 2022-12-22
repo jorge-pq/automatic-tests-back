b73d9cf6bfe0d228b3290bb91f934b32c11d1086
djorg-solutions <djorg90@gmail.com>
2022-12-22 12:02:03 -0500

    add client endpoint

--- src/controllers/clients.js
+++ src/controllers/clients.js
@@ -1,6 +1,9 @@
 const Auth = require('../middleware/auth.middleware');
 const Client = require("../models/Client");
+const Tenant = require("../models/Tenant");
 const {path} = require("../config");
+const {saveClient} = require('../service/client.service');
+
 
 module.exports = function(app){
 
@@ -8,6 +11,13 @@ app.get(path("clients"), Auth, async (req, res) => {
 	let data = req.body;
 	let clients = await Client.find({ tenant: data.tenant });
 	res.send(clients);
+})
+
+app.post(path("clients/create"), Auth, async (req, res) => {
+	let data = req.body;
+	const tenant = await Tenant.findOne({ _id: data.tenant });
+	await saveClient(data, tenant);
+	res.send({message: 'Client added succesfully!'});
 })
 
 }
